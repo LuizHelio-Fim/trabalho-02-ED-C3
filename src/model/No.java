2b1ccaf749898c4f9867f8b227d22826d644d3e9
Luiz Hélio <luizhelio.fim@outlook.com>
2026-05-29 15:46:36 -0300

    Update No.java

--- src/model/No.java
+++ src/model/No.java
@@ -1,5 +1,36 @@
 package model;
 
 public class No {
-
+	private Cliente info; 
+	private No esq, dir;
+	
+	public No(Cliente elem){
+		this.info = elem;
+		this.esq = null;
+		this.dir = null;
+	}
+	
+	public No getEsq(){
+		return this.esq;
+	}
+	
+	public No getDir(){
+		return this.dir;
+	}
+	
+	public Cliente getInfo(){
+		return this.info;
+	}
+	
+	public void setEsq(No no){
+		this.esq = no;
+	}
+	
+	public void setDir(No no){
+		this.dir = no;
+	}
+	
+	public void setInfo(Cliente elem){
+		this.info = elem;
+	}
 }
